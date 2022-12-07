e3ca8fb5ccda18c0469e8ee62908b476b8d457c6
Jamal <jayjamal86@gmail.com>
2022-12-07 20:55:13 +0300

    added a delete method to the base_model and attributes

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,10 @@
 from datetime import datetime
 import uuid
 import models
+from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -11,7 +15,14 @@ class BaseModel:
 
     This class will define all common.
     attributes/methods for other classes.
+    Attributes:
+        id: primary key, string of 60 characters, unique, can’t be null
+        created_at: datetime, can’t be null, default = current datetime
+        updated_at: datetime, can’t be null, default = current datetime
     """
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """__init__ constructor method for BaseModel class."""
@@ -25,7 +36,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            # models.storage.new(self) - moved to save method
+        if not self.id:
+            self.id = str(uuid.uuid4())
+        d = datetime.now()
+        if not self.created_at:
+            self.created_at = d
+            self.updated_at = d
+        if not self.updated_at:
+            self.updated_at = d
 
     def __str__(self):
         """__str__ method to print a string representation of the class."""
@@ -44,4 +63,10 @@ class BaseModel:
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         new_dict['__class__'] = self.__class__.__name__
+        if '_sa_instance_state' in new_dict:
+            del new_dict['_sa_instance_state']
         return new_dict
+
+    def delete(self):
+        """Delete method to delete the current instance from the storage."""
+        models.storage.delete(self)
